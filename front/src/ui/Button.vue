eabc6bb1d1322835365354671ab989dc89454c50
hugo-glt <hg.gilet@gmail.com>
2026-09-10 00:10:30 +0200

    ADD: Submit button usable

--- front/src/ui/Button.vue
+++ front/src/ui/Button.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-
+const emit = defineEmits<{ (e: 'click'): void }>()
 </script>
 
 <template>
-    <section>
-        <div>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white w-24 h-10 text-base font-bold py-2 px-2 rounded">
-                submit
-            </button>
-        </div>
-    </section>
+    <button
+        class="bg-blue-500 hover:bg-blue-700 text-white w-24 h-10 text-base font-bold py-2 px-2 rounded"
+        @click="emit('click')">submit
+    </button>
 </template>
--- front/src/views/GameView.vue
+++ front/src/views/GameView.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import GameGrid from '../components/game/GameGrid.vue'
 import Button from '../ui/Button.vue'
 
+const gameGrid = ref<InstanceType<typeof GameGrid> | null>(null)
 </script>
 
 <template>
-    <section class="flex flex-col items-center gap-2">
-        <div>
-            <GameGrid />
-        </div>
-    </section>
-
-    <section class="flex items-center justify-center text-4xl font-bold mt-5">
-        <Button />
+    <section class="flex flex-col items-center gap-5">
+        <GameGrid ref="gameGrid" />
+        <Button @click="gameGrid?.valider()" />
     </section>
 </template>

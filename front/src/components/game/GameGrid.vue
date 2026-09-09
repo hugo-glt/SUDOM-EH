a62ca8041a4dcf34247203048b1fc7eafc247001
hugo-glt <hg.gilet@gmail.com>
2026-09-09 16:24:58 +0200

    ADD: General Vue Pages

---
+++ front/src/components/game/GameGrid.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section>
+        <div class="flex justify-center p-2">
+            <div class="grid grid-cols-8 border-2 rounded-2xl border-black">
+                <div v-for="i in 40" :key="i"
+                    class="w-12 h-12 border rounded-xl border-black flex items-center justify-center text-lg font-bold">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,8 @@
+import "./style.css";
 import dictionnaireRaw from '@/data/noun.csv?raw'
 import { createApp } from "vue";
 import App from "./App.vue";
+
 
 createApp(App).mount("#app");
 
